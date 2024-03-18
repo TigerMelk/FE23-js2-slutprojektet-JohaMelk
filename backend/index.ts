06f15fe01c54bc14225e71dfd46e90b6ffb4e349
Melker Dahl <96880867+TigerMelk@users.noreply.github.com>
2024-03-18 15:06:34 +0100

    test backend filer

---
+++ backend/index.ts
@@ -0,0 +1,23 @@
+console.log("ayooo");
+import express from "express";
+import cors from "cors";
+import * as db from "./src/handledatabase.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+// app.get()
+// app.post()
+// app.put()
+// app.patch()
+// app.delete()
+
+app.listen(3000, () => {
+  console.log("listening to port 3000");
+});
