de1d9945283d278bcfe3bc09bfa9ecf12ab1ced1
Melker Dahl <jaghetermelker@gmail.com>
2024-03-25 14:50:50 +0100

    get user comments or posts

--- backend/src/handledatabase.ts
+++ backend/src/handledatabase.ts
@@ -5,35 +5,35 @@ import { error } from "console";
 import { match } from "assert";
 //database read and write
 async function readDatabase() {
-	const rawDbBuffer: Buffer = await fs.readFile("./src/db.json");
-	const rawDb: string = rawDbBuffer.toString("utf-8");
-	return JSON.parse(rawDb);
+  const rawDbBuffer: Buffer = await fs.readFile("./src/db.json");
+  const rawDb: string = rawDbBuffer.toString("utf-8");
+  return JSON.parse(rawDb);
 }
 async function writeDatabase(users: User[]): Promise<void> {
-	const db = { users };
-	await fs
-		.writeFile("./src/db.json", JSON.stringify(db, null, 2))
-		.then(() => {
-			console.log("database updated correctly");
-		})
-		.catch((error) => {
-			console.error("error writing database", error);
-		});
+  const db = { users };
+  await fs
+    .writeFile("./src/db.json", JSON.stringify(db, null, 2))
+    .then(() => {
+      console.log("database updated correctly");
+    })
+    .catch((error) => {
+      console.error("error writing database", error);
+    });
 }
 //
 // get functions
 // get all users
 async function getUsers(): Promise<User[]> {
-	const db = await readDatabase();
-	return db.users;
+  const db = await readDatabase();
+  return db.users;
 }
 // get individuel user
 // url id is user.id
 async function getUser(id: string): Promise<User | { message: string }> {
-	const users = await getUsers();
-	const user = users.find((user: User) => user.id == id);
-	if (user) return user;
-	else return { message: "user not found" };
+  const users = await getUsers();
+  const user = users.find((user: User) => user.id == id);
+  if (user) return user;
+  else return { message: "user not found" };
 }
 // get login //! funkar men är capslock sensitive
 // json format for this is
@@ -42,19 +42,19 @@ async function getUser(id: string): Promise<User | { message: string }> {
 //     "password": "password"
 // }
 async function logIn(
-	userName: string,
-	userPassword: string
+  userName: string,
+  userPassword: string
 ): Promise<User | { message: string }> {
-	const users = await getUsers();
-	for (const user of users) {
-		console.log(user);
+  const users = await getUsers();
+  for (const user of users) {
+    console.log(user);
 
-		if (user.name === userName && user.password === userPassword) {
-			console.log(user);
-			return user;
-		}
-	}
-	return { message: "user not found" };
+    if (user.name === userName && user.password === userPassword) {
+      console.log(user);
+      return user;
+    }
+  }
+  return { message: "user not found" };
 }
 
 // get individuell post //!funkar
@@ -62,44 +62,53 @@ async function logIn(
 // {"userId":"user.id or userId",
 // "postId":"posts.postId"}
 async function getPost(
-	userId: string,
-	postId: string
+  userId: string,
+  postId: string
 ): Promise<Post | { message: string }> {
-	//!fixa return
-	const user = await getUser(userId);
-	if ("posts" in user) {
-		for (const post of user.posts) {
-			if (post.postId === postId) {
-				return post;
-			}
-		}
-	}
-	return { message: "post not found in user " };
+  //!fixa return
+  const user = await getUser(userId);
+  if ("posts" in user) {
+    for (const post of user.posts) {
+      if (post.postId === postId) {
+        return post;
+      }
+    }
+  }
+  return { message: "post not found in user " };
 }
 // get posts with same category //! behöver testas
 async function getCategory(
-	category: Array<"game1" | "game2" | "game3">
+  category: Array<"game1" | "game2" | "game3">
 ): Promise<Post[]> {
-	const users = await getUsers();
-	const matchingPosts: Post[] = [];
-	for (const user of users) {
-		for (const post of user.posts) {
-			if (post.category === category) {
-				matchingPosts.push(post);
-			}
-		}
-	}
-	return matchingPosts;
+  const users = await getUsers();
+  const matchingPosts: Post[] = [];
+  for (const user of users) {
+    for (const post of user.posts) {
+      if (post.category === category) {
+        matchingPosts.push(post);
+      }
+    }
+  }
+  return matchingPosts;
 }
-// get comments eller posts //! behöver testas
+// get comments eller posts //! funkar
+// json format for this is:
+// {
+//     "userId": "user.id",
+//     "dataType": "posts"| "comments"
+// }
 async function getUserData(
-	user: User,
-	data: "comments" | "posts"
+  userId: string,
+  dataType: string
 ): Promise<Comment[] | Post[] | { message: string }> {
-	const foundUser = await getUser(user.id);
-	if ("comments" in foundUser && "posts" in foundUser) {
-		return data === "comments" ? foundUser.comments : foundUser.posts;
-	} else return { message: "no data found" };
+  const user = await getUser(userId);
+  if ("comments" in user && "posts" in user) {
+    if (dataType === "comments") {
+      return user.comments;
+    } else if (dataType === "posts") {
+      return user.posts;
+    }
+  }
 }
 
 // Post functions
@@ -112,28 +121,28 @@ async function getUserData(
 //     "image":"src for img"
 // }
 async function addUser(user: User): Promise<User> {
-	const users = await getUsers();
+  const users = await getUsers();
 
-	const existingName = users.find(
-		(existingName) => existingName.name === user.name
-	);
-	if (existingName) {
-		throw new Error("User with that name already exists");
-	}
+  const existingName = users.find(
+    (existingName) => existingName.name === user.name
+  );
+  if (existingName) {
+    throw new Error("User with that name already exists");
+  }
 
-	const id = crypto.randomUUID();
-	const newUser: User = {
-		id,
-		name: user.name,
-		password: user.password,
-		image: user.image,
-		admin: false,
-		comments: [],
-		posts: [],
-	};
-	users.push(newUser);
-	await writeDatabase(users);
-	return newUser;
+  const id = crypto.randomUUID();
+  const newUser: User = {
+    id,
+    name: user.name,
+    password: user.password,
+    image: user.image,
+    admin: false,
+    comments: [],
+    posts: [],
+  };
+  users.push(newUser);
+  await writeDatabase(users);
+  return newUser;
 }
 
 // Patch functions
@@ -148,16 +157,16 @@ async function addUser(user: User): Promise<User> {
 //     "comments": []
 // }
 async function addPost(userId: string, post: Post): Promise<Post> {
-	const newPost: Post = { postId: crypto.randomUUID(), userId, ...post };
-	const users = await getUsers();
-	for (const user of users) {
-		if (user.id == userId) {
-			user.posts.push(newPost);
-			break;
-		}
-	}
-	await writeDatabase(users);
-	return newPost;
+  const newPost: Post = { postId: crypto.randomUUID(), userId, ...post };
+  const users = await getUsers();
+  for (const user of users) {
+    if (user.id == userId) {
+      user.posts.push(newPost);
+      break;
+    }
+  }
+  await writeDatabase(users);
+  return newPost;
 }
 //add new comment to post. comments array and user.comments array //!funkar
 // json format for this is:
@@ -167,97 +176,97 @@ async function addPost(userId: string, post: Post): Promise<Post> {
 //	"commentText": "the comment"
 //}
 async function addComment(
-	userId: string,
-	postId: string,
-	commentText: string
+  userId: string,
+  postId: string,
+  commentText: string
 ): Promise<Comment> {
-	const commentId = crypto.randomUUID();
-	const users = await getUsers();
-	const userAddingComment = users.find((user) => user.id === userId);
-	const postOwner = users.find((user) =>
-		user.posts.some((post) => post.postId === postId)
-	);
-	const post = postOwner.posts.find((post) => post.postId === postId);
-	const newComment: Comment = {
-		commentId,
-		userId,
-		postId,
-		comment: commentText,
-	};
-	post.comments.push(newComment);
-	userAddingComment.comments.push(newComment);
-	await writeDatabase(users);
-	return newComment;
+  const commentId = crypto.randomUUID();
+  const users = await getUsers();
+  const userAddingComment = users.find((user) => user.id === userId);
+  const postOwner = users.find((user) =>
+    user.posts.some((post) => post.postId === postId)
+  );
+  const post = postOwner.posts.find((post) => post.postId === postId);
+  const newComment: Comment = {
+    commentId,
+    userId,
+    postId,
+    comment: commentText,
+  };
+  post.comments.push(newComment);
+  userAddingComment.comments.push(newComment);
+  await writeDatabase(users);
+  return newComment;
 }
 
 // delete functions
 // delete user from database //! funkar
 // url id is user.id
 async function deleteUser(id: string): Promise<void> {
-	const user = await getUser(id);
-	if (!user) throw new Error("User not found");
-	const users = await getUsers();
-	const updatedDatabase = users.filter((u) => u.id !== id);
-	await writeDatabase(updatedDatabase);
+  const user = await getUser(id);
+  if (!user) throw new Error("User not found");
+  const users = await getUsers();
+  const updatedDatabase = users.filter((u) => u.id !== id);
+  await writeDatabase(updatedDatabase);
 }
 
 // delete post from user.posts array // ! behöver testas
 async function deletePost(userId: string, postId: string): Promise<void> {
-	const user = await getUser(userId);
-	if (!user) throw new Error("User not found");
-	if (!("posts" in user)) {
-		throw new Error("User posts not found");
-	}
-	const postIndex = user.posts.findIndex((post) => post.postId === postId);
-	if (postIndex === -1) throw new Error("Post not found");
-	user.posts.splice(postIndex, 1);
-	await writeDatabase(await getUsers());
+  const user = await getUser(userId);
+  if (!user) throw new Error("User not found");
+  if (!("posts" in user)) {
+    throw new Error("User posts not found");
+  }
+  const postIndex = user.posts.findIndex((post) => post.postId === postId);
+  if (postIndex === -1) throw new Error("Post not found");
+  user.posts.splice(postIndex, 1);
+  await writeDatabase(await getUsers());
 }
 
 // delete comment from post.comments array and user.comments array //! behöver testas
 async function deleteComment(commentId: string): Promise<void> {
-	const users = await getUsers();
-	let commentDeleted = false;
+  const users = await getUsers();
+  let commentDeleted = false;
 
-	for (const user of users) {
-		const commentIndex = user.comments.findIndex(
-			(comment) => comment.commentId === commentId
-		);
-		if (commentIndex !== -1) {
-			user.comments.splice(commentIndex, 1);
-			commentDeleted = true;
-		}
-		for (const post of user.posts) {
-			const postCommentIndex = post.comments.findIndex(
-				(comment) => commentId === comment.commentId
-			);
+  for (const user of users) {
+    const commentIndex = user.comments.findIndex(
+      (comment) => comment.commentId === commentId
+    );
+    if (commentIndex !== -1) {
+      user.comments.splice(commentIndex, 1);
+      commentDeleted = true;
+    }
+    for (const post of user.posts) {
+      const postCommentIndex = post.comments.findIndex(
+        (comment) => commentId === comment.commentId
+      );
 
-			if (postCommentIndex !== -1) {
-				post.comments.splice(postCommentIndex, 1);
-				commentDeleted = true;
-			}
-		}
-	}
-	if (commentDeleted === true) {
-		await writeDatabase(users);
-		return;
-	}
-	throw new Error("Comment not found");
+      if (postCommentIndex !== -1) {
+        post.comments.splice(postCommentIndex, 1);
+        commentDeleted = true;
+      }
+    }
+  }
+  if (commentDeleted === true) {
+    await writeDatabase(users);
+    return;
+  }
+  throw new Error("Comment not found");
 }
 
 export {
-	readDatabase,
-	writeDatabase,
-	getUser,
-	getUsers,
-	getUserData,
-	addUser,
-	addComment,
-	addPost,
-	deleteUser,
-	deletePost,
-	deleteComment,
-	getCategory,
-	logIn,
-	getPost,
+  readDatabase,
+  writeDatabase,
+  getUser,
+  getUsers,
+  getUserData,
+  addUser,
+  addComment,
+  addPost,
+  deleteUser,
+  deletePost,
+  deleteComment,
+  getCategory,
+  logIn,
+  getPost,
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,85 +6,94 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
 
 // getUser eller Users
 app.get("/api/:id?", (req, res) => {
-	console.log("get req recieved");
+  console.log("get req recieved");
 
-	if (req.params.id) {
-		db.getUser(req.params.id).then((user) => {
-			if (user) res.json(user);
-		});
-	} else {
-		db.getUsers().then((users) => {
-			if (users) res.json(users);
-		});
-	}
+  if (req.params.id) {
+    db.getUser(req.params.id).then((user) => {
+      if (user) res.json(user);
+    });
+  } else {
+    db.getUsers().then((users) => {
+      if (users) res.json(users);
+    });
+  }
 });
 //! funkar
 // login
 app.get("/api/login/user", (req, res) => {
-	const { name, password } = req.body;
-	console.log(req.body);
-	if (!name || !password) {
-		throw new Error("Name and password required");
-	}
-	db.logIn(name, password).then((user) => {
-		if (user) res.json(user);
-		else throw new Error("user not found");
-	});
+  const { name, password } = req.body;
+  console.log(req.body);
+  if (!name || !password) {
+    throw new Error("Name and password required");
+  }
+  db.logIn(name, password).then((user) => {
+    if (user) res.json(user);
+    else throw new Error("user not found");
+  });
 });
-// getPost
+// getPost //! funkar
 app.get("/api/user/post", (req, res) => {
-	const { userId, postId } = req.body;
-	db.getPost(userId, postId).then((post) => res.json(post));
+  const { userId, postId } = req.body;
+  db.getPost(userId, postId).then((post) => res.json(post));
+});
+
+// get all comments or posts from user
+app.get("/api/user/data/", (req, res) => {
+  const { userId, dataType } = req.body;
+  console.log(userId, dataType);
+  db.getUserData(userId, dataType).then((userData) => {
+    res.json(userData);
+  });
 });
 //! denna funkar :)
 // addUser
 app.post("/api/users", (req, res) => {
-	const { name, password, image } = req.body;
-	const newUser = {
-		id: "",
-		name,
-		password,
-		image,
-		admin: false,
-		comments: [],
-		posts: [],
-	};
-	const addedUser = db.addUser(newUser).then(() => res.json(addedUser));
+  const { name, password, image } = req.body;
+  const newUser = {
+    id: "",
+    name,
+    password,
+    image,
+    admin: false,
+    comments: [],
+    posts: [],
+  };
+  const addedUser = db.addUser(newUser).then(() => res.json(addedUser));
 });
 // app.put()
 // addPost
 //! funkar
 app.patch("/api/:id?/post/", (req, res) => {
-	console.log("patch req recieved for post");
-	if (req.params.id) {
-		db.addPost(req.params.id, req.body).then((post) => {
-			res.json(post);
-		});
-	}
+  console.log("patch req recieved for post");
+  if (req.params.id) {
+    db.addPost(req.params.id, req.body).then((post) => {
+      res.json(post);
+    });
+  }
 });
 //! borde funka..
 // addComment
 app.patch("/api/post/comment", (req, res) => {
-	console.log("patch req recieved for comment");
-	const { userId, postId, commentText } = req.body;
-	db.addComment(userId, postId, commentText).then((newComment) =>
-		res.json(newComment)
-	);
+  console.log("patch req recieved for comment");
+  const { userId, postId, commentText } = req.body;
+  db.addComment(userId, postId, commentText).then((newComment) =>
+    res.json(newComment)
+  );
 });
 //
 // delete user //!funkar
 app.delete("/api/users/:id", (req, res) => {
-	const userId = req.params.id;
-	db.deleteUser(userId).then(() => res.json({ message: "User Deleted" }));
+  const userId = req.params.id;
+  db.deleteUser(userId).then(() => res.json({ message: "User Deleted" }));
 });
 
 app.listen(3000, () => {
-	console.log("listening to port 3000");
+  console.log("listening to port 3000");
 });
