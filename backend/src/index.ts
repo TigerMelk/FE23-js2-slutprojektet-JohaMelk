1c5ba7d7dbf0c1cf43e4e5f271568c992f078b11
johannaostling <johanna.ostling@gritacademy.se>
2024-03-27 11:32:49 +0100

    small fixes with database and addcomment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import * as db from "./handledatabase.js";
 import { error } from "console";
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
