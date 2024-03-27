9d8a62532d769b36e673073af1df70da827a05b9
JavadJananeh <150336284+JavadJananeh@users.noreply.github.com>
2024-03-27 12:25:10 +0100

    update

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -43,7 +43,7 @@ commentsBtn.addEventListener('click', () =>{
 // Function to handle page transition from Login to Signup
 function goToSignup(): void {
     const login = document.getElementById('login');
-    const signup = document.getElementById('signup');
+    const signup = document.getElementById('signup'); 
 
     if (login && signup) {
         login.style.display = 'none';
@@ -54,7 +54,7 @@ function goToSignup(): void {
 // Function to handle page transition from Signup to Login
 function goToLogin(): void {
     const login = document.getElementById('login');
-    const signup = document.getElementById('signup');
+    const signup = document.getElementById('signup'); 
 
     if (login && signup) {
         login.style.display = 'block';
@@ -64,4 +64,5 @@ function goToLogin(): void {
 
 // Event listeners to trigger page transitions
 document.getElementById('goToSignup')?.addEventListener('click', goToSignup);
-document.getElementById('goTologin')?.addEventListener('click', goToLogin);
+document.getElementById('goToLogin')?.addEventListener('click', goToLogin); 
+
