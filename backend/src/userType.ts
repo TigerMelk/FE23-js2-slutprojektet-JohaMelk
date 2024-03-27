918d3323020f9037603e67d07f765984ba363b92
Melker Dahl <jaghetermelker@gmail.com>
2024-03-27 16:13:05 +0100

    backend fix. login e rätt nu

--- backend/src/userType.ts
+++ backend/src/userType.ts
@@ -1,25 +1,27 @@
 export type User = {
-	id: string;
-	name: string;
-	password: string;
-	image: string;
-	admin: boolean;
-	comments: Array<Comment>;
-	posts: Array<Post>;
+  id: string;
+  name: string;
+  password: string;
+  image: string;
+  admin: boolean;
+  comments: Array<Comment>;
+  posts: Array<Post>;
 };
+export type UserWithoutPassword = Omit<User, "password">;
+
 export type Comment = {
-	commentId: string;
-	postId: string;
-	userId: string;
-	comment: string;
-	//   likes: number
+  commentId: string;
+  postId: string;
+  userId: string;
+  comment: string;
+  //   likes: number
 };
 export type Post = {
-	postId: string;
-	userId: string;
-	title: string;
-	bread: string;
-	category: ["game1", "game2", "game3"];
-	comments: Array<Comment>;
-	//   likes: number;
+  postId: string;
+  userId: string;
+  title: string;
+  bread: string;
+  category: ["game1", "game2", "game3"];
+  comments: Array<Comment>;
+  //   likes: number;
 };
