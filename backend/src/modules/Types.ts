28c5317c8e190dd3ae164155df841694c09a7fc7
TigerMelk <96880867+TigerMelk@users.noreply.github.com>
2024-04-05 13:04:52 +0200

    timestamp fix on posts
    getCategory now returns array of post in chronological order

--- backend/src/modules/Types.ts
+++ backend/src/modules/Types.ts
@@ -1,12 +1,12 @@
 // Type for User, how each object in the Users array should look like in db.json
 export type User = {
-  id: string;
-  name: string;
-  password: string;
-  image: string;
-  admin: boolean;
-  comments: Array<Comment>;
-  posts: Array<Post>;
+	id: string;
+	name: string;
+	password: string;
+	image: string;
+	admin: boolean;
+	comments: Array<Comment>;
+	posts: Array<Post>;
 };
 
 // Same as type User, but used for sending the data back to the clientside, excluding the user's password
@@ -14,20 +14,21 @@ export type UserWithoutPassword = Omit<User, "password">;
 
 // How each post object in the posts array should look like
 export type Post = {
-  postId: string;
-  userId: string;
-  title: string;
-  bread: string;
-  category: ["League of Legends", "Bloodborne", "Palworld"];
-  comments: Array<Comment>;
-  //   likes: number;
+	postId: string;
+	userId: string;
+	title: string;
+	bread: string;
+	timestamp: number;
+	category: ["League of Legends", "Bloodborne", "Palworld"];
+	comments: Array<Comment>;
+	//   likes: number;
 };
 
 // How each comment object in the comments, as well as post.comments, array should look like
 export type Comment = {
-  commentId: string;
-  postId: string;
-  userId: string;
-  comment: string;
-  //   likes: number
+	commentId: string;
+	postId: string;
+	userId: string;
+	comment: string;
+	//   likes: number
 };
--- backend/src/modules/posts.ts
+++ backend/src/modules/posts.ts
@@ -2,56 +2,71 @@ import { writeDatabase } from "./handledatabase.js";
 import { getUser, getUsers } from "./users.js";
 import { Post } from "./Types.js";
 async function getPost(
-  userId: string,
-  postId: string
+	userId: string,
+	postId: string
 ): Promise<Post | { message: string }> {
-  const user = await getUser(userId);
-  if ("posts" in user) {
-    for (const post of user.posts) {
-      if (post.postId === postId) {
-        return post;
-      }
-    }
-  }
-  return { message: "post not found in user " };
+	const user = await getUser(userId);
+	if ("posts" in user) {
+		for (const post of user.posts) {
+			if (post.postId === postId) {
+				return post;
+			}
+		}
+	}
+	return { message: "post not found in user " };
 }
 
 async function getCategory(
-  category: Array<"League of Legends" | "Bloodborne" | "Palworld">
+	category: Array<"League of Legends" | "Bloodborne" | "Palworld">
 ): Promise<Post[]> {
-  const users = await getUsers();
-  const matchingPosts: Post[] = [];
-  for (const user of users) {
-    for (const post of user.posts) {
-      if (post.category === category) {
-        matchingPosts.push(post);
-      }
-    }
-  }
-  return matchingPosts;
+	const users = await getUsers();
+	const matchingPosts: Post[] = [];
+	for (const user of users) {
+		for (const post of user.posts) {
+			if (post.category === category) {
+				matchingPosts.push(post);
+			}
+		}
+	}
+	console.log(matchingPosts);
+	matchingPosts.sort((a, b) => {
+		const dateA = new Date(a.timestamp);
+		const dateB = new Date(b.timestamp);
+
+		if (dateA < dateB) return -1;
+		if (dateA > dateB) return 1;
+		return 0;
+	});
+	console.log(matchingPosts);
+	return matchingPosts;
 }
 async function addPost(userId: string, post: Post): Promise<Post> {
-  const newPost: Post = { postId: crypto.randomUUID(), userId, ...post };
-  const users = await getUsers();
-  for (const user of users) {
-    if (user.id == userId) {
-      user.posts.push(newPost);
-      break;
-    }
-  }
-  await writeDatabase(users);
-  return newPost;
+	const newPost: Post = {
+		postId: crypto.randomUUID(),
+		userId,
+		timestamp: Date.now(),
+		...post,
+	};
+	const users = await getUsers();
+	for (const user of users) {
+		if (user.id == userId) {
+			user.posts.push(newPost);
+			break;
+		}
+	}
+	await writeDatabase(users);
+	return newPost;
 }
 async function deletePost(userId: string, postId: string): Promise<void> {
-  const users = await getUsers();
-  const userIndex = users.findIndex((user) => user.id === userId);
-  if ("posts" in users[userIndex]) {
-    users[userIndex].posts = users[userIndex].posts.filter(
-      (post) => post.postId !== postId
-    );
-    await writeDatabase(users);
-    console.log("success");
-  }
+	const users = await getUsers();
+	const userIndex = users.findIndex((user) => user.id === userId);
+	if ("posts" in users[userIndex]) {
+		users[userIndex].posts = users[userIndex].posts.filter(
+			(post) => post.postId !== postId
+		);
+		await writeDatabase(users);
+		console.log("success");
+	}
 }
 
 export { getPost, getCategory, addPost, deletePost };
