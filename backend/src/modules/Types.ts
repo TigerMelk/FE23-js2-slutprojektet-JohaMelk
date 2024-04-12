8d335324c6730bc1b0126c39bcf67d2dabb98937
Melker Dahl <jaghetermelker@gmail.com>
2024-04-12 14:41:57 +0200

    merge 2

---
+++ backend/src/modules/Types.ts
@@ -0,0 +1,36 @@
+// Type for User, how each object in the Users array should look like in db.json
+export type User = {
+	id: string;
+	name: string;
+	password: string;
+	image: string;
+	admin: boolean;
+	comments: Array<Comment>;
+	posts: Array<Post>;
+};
+
+// Same as type User, but used for sending the data back to the clientside, excluding the user's password
+export type UserWithoutPassword = Omit<User, "password">;
+
+// How each post object in the posts array should look like
+export type Post = {
+	postId: string;
+	userId: string;
+	name: string;
+	title: string;
+	bread: string;
+	timestamp: number;
+	category: ["League of Legends", "Bloodborne", "Palworld"];
+	comments: Array<Comment>;
+	//   likes: number;
+};
+
+// How each comment object in the comments, as well as post.comments, array should look like
+export type Comment = {
+	commentId: string;
+	postId: string;
+	userId: string;
+	name: string;
+	comment: string;
+	//   likes: number
+};
---
+++ backend/src/modules/handledatabase.ts
@@ -0,0 +1,20 @@
+import fs from "fs/promises";
+import { User } from "./Types.js";
+async function readDatabase() {
+  const rawDbBuffer: Buffer = await fs.readFile("./src/storage/db.json");
+  const rawDb: string = rawDbBuffer.toString("utf-8");
+  return JSON.parse(rawDb);
+}
+async function writeDatabase(users: User[]): Promise<void> {
+  const db = { users };
+  await fs
+    .writeFile("./src/storage/db.json", JSON.stringify(db, null, 2))
+    .then(() => {
+      console.log("database updated correctly");
+    })
+    .catch((error) => {
+      console.error("error writing database", error);
+    });
+}
+
+export { readDatabase, writeDatabase };
